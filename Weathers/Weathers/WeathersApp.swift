cea0a335eef60e0df176076fbf864ed44d0cee3d
Nt-w1 <redblue_desk@yahoo.co.jp>
2023-08-15 12:45:59 +0900

    init commit

---
+++ Weathers/Weathers/WeathersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathersApp.swift
+//  Weathers
+//
+//  Created by 永瀬 on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct WeathersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
